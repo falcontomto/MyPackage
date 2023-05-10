88491d162721da9c580ac0c6fe22008f65ec22ab
Yat To <yat.to@incrowdsports.com>
2023-05-10 15:49:06 +0100

    done

---
+++ Tests/MyPackageTests/MyPackageTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import MyTarget
+
+final class MyPackageTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertEqual(String.helloWorld, "Hello World!")
+    }
+}
