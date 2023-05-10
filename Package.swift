ae7420d5840032b0206f913591010a377305cb67
Yat To <yat.to@incrowdsports.com>
2023-05-10 16:29:55 +0100

    add other package

--- Package.swift
+++ Package.swift
@@ -14,8 +14,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "MyTarget", dependencies: ["MyOtherTarget"]),
         .target(
-            name: "MyTarget"),
+            name: "MyOtherTarget"),
         .testTarget(
             name: "MyPackageTests",
             dependencies: ["MyTarget"]),
---
+++ Sources/MyOtherTarget/MyOtherPackage.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yat To on 10/05/2023.
+//
+
+import Foundation
+
+public extension Int {
+    static let deadBeef = 0xDEADBEEF
+}
